5bdfe3f426582ce0beafcb9b27436be781d64ab8
Marlon <martianworm17@gmail.com>
2018-03-13 21:49:56 +0800

    Using Variables

--- src/Application.java
+++ src/Application.java
@@ -2,7 +2,26 @@
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		int   myNumber = 88;
+		short myShort  = 847;
+		long  myLong   = 9797;
+		
+		double myDouble = 7.32434;
+		float  myFloat  = 324.3f;
+		
+		char myChar = 'y';
+		boolean myBoolean = false;
+		
+		byte myByte = 127;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
 	}
 
 }
