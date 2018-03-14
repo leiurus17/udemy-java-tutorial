cd6fd2ae49617b5a2db2d521ff6b5f9f42dd1b61
Marlon <martianworm17@gmail.com>
2018-03-14 16:31:46 +0800

    For Loops

---
+++ src/com/kamotelabs/tutorial5/Application.java
@@ -0,0 +1,12 @@
+package com.kamotelabs.tutorial5;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.printf("The value of i is: %d\n", i);
+		}
+	}
+
+}
