dcb1f6e3b843fca85fc3a5ebe5a40f484090dc38
Marlon <martianworm17@gmail.com>
2018-03-14 14:51:11 +0800

    Playing with boolean variable

---
+++ src/com/kamotelabs/tutorial4/Application.java
@@ -0,0 +1,14 @@
+package com.kamotelabs.tutorial4;
+
+public class Application {
+	
+	public static void main(String[] args) {
+
+		int value = 10;
+		
+		boolean loop = value < 20;
+		
+		System.out.println(loop);
+		
+	}
+}
