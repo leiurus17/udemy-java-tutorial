5b2033b25bc91951e5a038593c00c822a61b1cd0
Marlon <martianworm17@gmail.com>
2018-03-14 14:52:44 +0800

    While Loops

--- src/com/kamotelabs/tutorial4/Application.java
+++ src/com/kamotelabs/tutorial4/Application.java
@@ -4,11 +4,13 @@ public class Application {
 	
 	public static void main(String[] args) {
 
-		int value = 10;
+		int value = 0;
 		
-		boolean loop = value < 20;
-		
-		System.out.println(loop);
+		while (value < 10) {
+			System.out.println("Hello " + value);
+			
+			value = value + 1;
+		}
 		
 	}
 }
