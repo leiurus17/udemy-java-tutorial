b0ba512a0e64f9d627b43ef78d5e8538b401bf3d
Marlon <martianworm17@gmail.com>
2018-03-19 17:33:16 +0800

    Arrays of Strings

--- src/com/kamotelabs/tutorial11/App.java
+++ src/com/kamotelabs/tutorial11/App.java
@@ -16,5 +16,21 @@ public class App {
 		for(String fruit: fruits) {
 			System.out.println(fruit);
 		}
+		
+		int value = 123;
+		
+		String text = null;
+		
+		System.out.println(text);
+		
+		String[] texts = new String[2];
+		
+		System.out.println(texts[0]);
+		
+		texts[0] = "one";
+		
+		System.out.println(texts[0]);
+		
+		
 	}
 }
