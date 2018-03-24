12e4540d5cc875971ea1151787ed2f276db2a6a8
Marlon <martianworm17@gmail.com>
2018-03-24 17:41:02 +0800

    Method Parameters

---
+++ src/com/kamotelabs/tutorial16/App.java
@@ -0,0 +1,18 @@
+package com.kamotelabs.tutorial16;
+
+class Robot {
+	public void speak() {
+		System.out.println("Hello");
+	}
+}
+
+public class App {
+	
+	public static void main(String[] args) {
+		Robot sam = new Robot();
+		
+		sam.speak();
+		
+	}
+
+}
