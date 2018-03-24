ae07d9ad9180492c0b0423adef949d27855a85a4
Marlon <martianworm17@gmail.com>
2018-03-24 17:43:50 +0800

    Method Parameters

--- src/com/kamotelabs/tutorial16/App.java
+++ src/com/kamotelabs/tutorial16/App.java
@@ -4,6 +4,10 @@ class Robot {
 	public void speak(String text) {
 		System.out.println(text);
 	}
+	
+	public void jump(int height) {
+		System.out.println("Jumping: " + height);
+	}
 }
 
 public class App {
@@ -12,7 +16,7 @@ public class App {
 		Robot sam = new Robot();
 		
 		sam.speak("Hi I'm Sam!");
-		
+		sam.jump(7);
 	}
 
 }
