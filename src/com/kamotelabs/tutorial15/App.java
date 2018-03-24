1e0168756ba3595fb5fd21b26803d3d3f5e2cced
Marlon <martianworm17@gmail.com>
2018-03-24 15:56:36 +0800

    Getters and Return Values

--- src/com/kamotelabs/tutorial15/App.java
+++ src/com/kamotelabs/tutorial15/App.java
@@ -8,10 +8,18 @@ class Person {
 		System.out.println("My name is: " + name);
 	}
 	
-	void calculateYearsToRetirement() {
+	int calculateYearsToRetirement() {
 		int yearsLeft = 65 - age;
 		
-		System.out.println(yearsLeft);
+		return yearsLeft;
+	}
+	
+	int getAge() {
+		return age;
+	}
+	
+	String getName() {
+		return name;
 	}
 }
 
@@ -24,7 +32,16 @@ public class App {
 		person1.age = 25;
 		
 		person1.speak();
-		person1.calculateYearsToRetirement();
+		
+		int years = person1.calculateYearsToRetirement();
+		
+		System.out.println("Years till retirement: " + years);
+		
+		int age = person1.getAge();
+		String name = person1.getName();
+		
+		System.out.println("Name is: " + name);
+		System.out.println("Age is: " + age);
 	}
 
 }
