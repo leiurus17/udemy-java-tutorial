88de26642e41abff01ab43fe64636988b902ff53
Marlon <martianworm17@gmail.com>
2018-03-24 17:46:32 +0800

    Method Parameters

--- src/com/kamotelabs/tutorial16/App.java
+++ src/com/kamotelabs/tutorial16/App.java
@@ -8,6 +8,10 @@ class Robot {
 	public void jump(int height) {
 		System.out.println("Jumping: " + height);
 	}
+	
+	public void move(String direction, double distance) {
+		System.out.println("Moving " + distance + " meters in direction " + direction);
+	}
 }
 
 public class App {
@@ -17,6 +21,7 @@ public class App {
 		
 		sam.speak("Hi I'm Sam!");
 		sam.jump(7);
+		sam.move("West", 12.2);
 	}
 
 }
