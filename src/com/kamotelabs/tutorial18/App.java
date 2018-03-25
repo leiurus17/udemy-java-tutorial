8e890c8f5b6ec4c8956fa16ad917e55d1958ee4a
Marlon <martianworm17@gmail.com>
2018-03-25 13:20:06 +0800

    Constructors

--- src/com/kamotelabs/tutorial18/App.java
+++ src/com/kamotelabs/tutorial18/App.java
@@ -1,10 +1,30 @@
 package com.kamotelabs.tutorial18;
 
 class Machine {
+	private String name;
+	private int code;
 	
 	public Machine () {
+		
 		System.out.println("Constructor running!");
+		
+		name = "Arnie";
 	}
+	
+	public Machine(String name) {
+		
+		System.out.println("Second constructor running!");
+		this.name = name;
+	}
+	
+	public Machine(String name, int code) {
+		
+		System.out.println("Third constructor running!");
+		this.name = name;
+		this.code = code;
+	}
+	
+	
 }
 
 public class App {
@@ -13,6 +33,9 @@ public class App {
 		Machine machine1 = new Machine();
 		
 		new Machine(); // This invoke an instance of object in itself
+		
+		Machine machine2 = new Machine("Bertie");
+		Machine machine3 = new Machine("Chalky", 7);
 	}
 
 }
