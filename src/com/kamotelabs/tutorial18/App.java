535b64844dd1445934d47690b784ea5370eebeeb
Marlon <martianworm17@gmail.com>
2018-03-25 13:07:13 +0800

    Constructors

---
+++ src/com/kamotelabs/tutorial18/App.java
@@ -0,0 +1,16 @@
+package com.kamotelabs.tutorial18;
+
+class Machine {
+	
+	public Machine () {
+		System.out.println("Constructor running!");
+	}
+}
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
