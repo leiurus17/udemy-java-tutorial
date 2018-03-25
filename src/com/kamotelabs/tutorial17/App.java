f76e36e82cf16f9366cf3fb83b01d34a157061f5
Marlon <martianworm17@gmail.com>
2018-03-25 12:38:48 +0800

    Setters and "this"

--- src/com/kamotelabs/tutorial17/App.java
+++ src/com/kamotelabs/tutorial17/App.java
@@ -4,6 +4,14 @@ class Frog {
 	String name;
 	int age;
 	
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	public void setAge(int newAge) {
+		age = newAge;
+	}
+	
 	public String getName() {
 		return name;
 	}
@@ -19,8 +27,11 @@ public class App {
 		
 		Frog frog1 = new Frog();
 		
-		frog1.name = "Bertie";
-		frog1.age = 1;
+		// frog1.name = "Bertie";
+		// frog1.age = 1;
+		
+		frog1.setName("Bertie");
+		frog1.setAge(7);
 		
 		System.out.println(frog1.getName());
 	}
