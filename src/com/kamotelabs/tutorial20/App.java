13c81b62c46da1f987fc3295fe60b39d7ec92a4f
Marlon <martianworm17@gmail.com>
2018-03-28 16:31:23 +0800

    String Builder and String Formatting

---
+++ src/com/kamotelabs/tutorial20/App.java
@@ -0,0 +1,16 @@
+package com.kamotelabs.tutorial20;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		String info = "";
+		
+		info += "My name is Bob.";
+		info += " ";
+		info += "I am a builder.";
+		
+		System.out.println(info);
+	}
+
+}
