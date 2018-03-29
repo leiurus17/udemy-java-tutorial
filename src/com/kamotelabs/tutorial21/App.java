c019c17bfb89e07af3fbbf3bc5456d67c1d69738
Marlon <martianworm17@gmail.com>
2018-03-29 22:14:34 +0800

    The toString Method

---
+++ src/com/kamotelabs/tutorial21/App.java
@@ -0,0 +1,41 @@
+package com.kamotelabs.tutorial21;
+
+class Frog {
+	
+	private int id;
+	private String name;
+	
+	public Frog(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(id)
+		.append(": ")
+		.append(name);
+		
+				
+				
+		return sb.toString();
+	}
+}
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		Object obj = new Object();
+		
+		System.out.println(obj.hashCode());
+		
+		Frog frog1 = new Frog(7, "Freddy");
+		Frog frog2 = new Frog(3, "Roger");
+		
+		System.out.println(frog1);
+		System.out.println(frog2);
+	}
+
+}
