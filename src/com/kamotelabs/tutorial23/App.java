ab42083a8af203d9560a401b824178812724c529
Marlon <martianworm17@gmail.com>
2018-03-30 11:33:01 +0800

    Packages

---
+++ src/com/kamotelabs/tutorial23/App.java
@@ -0,0 +1,13 @@
+package com.kamotelabs.tutorial23;
+
+import ocean.Fish;
+import ocean.Seaweed;
+
+public class App {
+	
+	public static void main(String[] args) {
+		Fish fish = new Fish();
+		Seaweed weed = new Seaweed();
+	}
+
+}
