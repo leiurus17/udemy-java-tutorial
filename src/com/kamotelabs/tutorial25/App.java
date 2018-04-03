9e169ce8f5e1f97976548383172cbf67421fbd09
Marlon <martianworm17@gmail.com>
2018-04-03 22:00:47 +0800

    Public, Private, Protected

---
+++ src/com/kamotelabs/tutorial25/App.java
@@ -0,0 +1,10 @@
+package com.kamotelabs.tutorial25;
+
+public class App {
+
+	public static void main(String[] args) {
+		Plant plant = new Plant();
+		System.out.println(plant.name);
+	}
+
+}
---
+++ src/com/kamotelabs/tutorial25/Plant.java
@@ -0,0 +1,10 @@
+package com.kamotelabs.tutorial25;
+
+public class Plant {
+	public String name;
+	
+	public Plant() {
+		name = "Freddy";
+	}
+
+}
