9282f0afc411e826de31b2a344dbea9ecf822630
Marlon <martianworm17@gmail.com>
2018-04-03 22:04:04 +0800

    Public, Private, Protected

--- src/com/kamotelabs/tutorial25/App.java
+++ src/com/kamotelabs/tutorial25/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		Plant plant = new Plant();
 		System.out.println(plant.name);
+		System.out.println(plant.ID);
 	}
 
 }
--- src/com/kamotelabs/tutorial25/Plant.java
+++ src/com/kamotelabs/tutorial25/Plant.java
@@ -1,7 +1,11 @@
 package com.kamotelabs.tutorial25;
 
 public class Plant {
+	// bad practice
 	public String name;
+	
+	// acceptable practice
+	public final static int ID = 8;
 	
 	public Plant() {
 		name = "Freddy";
