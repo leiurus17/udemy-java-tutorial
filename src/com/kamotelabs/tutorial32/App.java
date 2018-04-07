9abd2e6b7ba973e5ca2d085eca981ebee1f06307
Marlon <martianworm17@gmail.com>
2018-04-07 23:23:07 +0800

    Anonymous Classes

---
+++ src/com/kamotelabs/tutorial32/App.java
@@ -0,0 +1,20 @@
+package com.kamotelabs.tutorial32;
+
+class Machine {
+	public void start() {
+		System.out.println("Starting machine ...");
+	}
+}
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Machine machine1 = new Machine();
+		
+		machine1.start();
+		
+		
+	}
+
+}
