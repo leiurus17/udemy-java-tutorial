8499026db8602c5a414f26e485b1c93a2c4a6d0f
Marlon <martianworm17@gmail.com>
2018-04-07 18:51:05 +0800

    Generics and Wildcards

---
+++ src/com/kamotelabs/tutorial31/App.java
@@ -0,0 +1,31 @@
+package com.kamotelabs.tutorial31;
+
+import java.util.ArrayList;
+
+class Machine {
+	
+}
+
+class Camera extends Machine {
+	
+}
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> list = new ArrayList<>();
+		
+		list.add("one");
+		list.add("two");
+		
+		showList(list);
+	}
+	
+	public static void showList(ArrayList<String> list) {
+		for(String value: list) {
+			System.out.println(value);
+		}
+	}
+
+}
