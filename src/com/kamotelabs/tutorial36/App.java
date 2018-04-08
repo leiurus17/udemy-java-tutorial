523abe8460c949f12b61b3c1f0b247d63d6d0c87
Marlon <martianworm17@gmail.com>
2018-04-08 15:10:48 +0800

    Runtime vs. Checked Exceptions

--- src/com/kamotelabs/tutorial36/App.java
+++ src/com/kamotelabs/tutorial36/App.java
@@ -3,10 +3,33 @@ package com.kamotelabs.tutorial36;
 public class App {
 
 	public static void main(String[] args) {
-		
-		int value = 7;
-		
-		value = value / 0;
+
+		// java.lang.ArrayIndexOutOfBoundsException
+		try {
+			String[] texts = { "one", "two", "three" };
+			System.out.println(texts[3]);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// java.lang.NullPointerException
+		try {
+			String text = null;
+			System.out.println(text.length());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// java.lang.ArithmeticException
+		try {
+			int value = 7;
+			value = value / 0;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
