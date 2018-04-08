10b458cca80701498c7f1158166e12274e731057
Marlon <martianworm17@gmail.com>
2018-04-08 10:30:08 +0800

    Handling Exceptions

---
+++ src/com/kamotelabs/tutorial34_3/App.java
@@ -0,0 +1,21 @@
+package com.kamotelabs.tutorial34_3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class App {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		openFile();
+		
+	}
+	
+	public static void openFile() throws FileNotFoundException {
+		File file = new File("test.txt");
+		
+		FileReader fr = new FileReader(file);
+	}
+
+}
