9f646c330f78d71b501f0fe15f2df05cf86bec2f
Marlon <martianworm17@gmail.com>
2018-04-10 18:23:02 +0800

    Creating and Writing Text Files

--- src/com/kamotelabs/tutorial41/App.java
+++ src/com/kamotelabs/tutorial41/App.java
@@ -1,9 +1,28 @@
 package com.kamotelabs.tutorial41;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class App {
 
 	public static void main(String[] args) {
 
+		File file = new File("testwrite.txt");
+
+		try (BufferedWriter br = new BufferedWriter(new FileWriter(file))) {
+					
+			br.write("This is line one");
+			br.newLine();
+			br.write("This is line two");
+			br.newLine();
+			br.newLine();
+			br.write("Last line.");
+			
+		} catch (IOException e) {
+			System.out.println("Unable to read file " + file.toString());
+		}
 	}
 
 }
