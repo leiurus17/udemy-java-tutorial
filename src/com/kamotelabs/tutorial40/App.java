81a4de52c7db75c6895a5c8eba10d35b7ccbacda
Marlon <martianworm17@gmail.com>
2018-04-10 17:50:17 +0800

    Creating and Writing Text Files

---
+++ src/com/kamotelabs/tutorial40/App.java
@@ -0,0 +1,8 @@
+package com.kamotelabs.tutorial40;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Just a dummy class.");
+	}
+}
---
+++ src/com/kamotelabs/tutorial41/App.java
@@ -0,0 +1,9 @@
+package com.kamotelabs.tutorial41;
+
+public class App {
+
+	public static void main(String[] args) {
+
+	}
+
+}
