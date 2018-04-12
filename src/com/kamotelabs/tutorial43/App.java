1f35bec693cf8f2ba0fb056924bb7856c3b92e9d
Marlon <martianworm17@gmail.com>
2018-04-12 20:43:09 +0800

    Inner Classes

--- src/com/kamotelabs/tutorial43/App.java
+++ src/com/kamotelabs/tutorial43/App.java
@@ -7,6 +7,10 @@ public class App {
 		Robot robot = new Robot(7);
 		
 		robot.start();
+		
+		Robot.Legs legs = robot.new Legs();
+		
+		legs.run();
 	}
 
 }
--- src/com/kamotelabs/tutorial43/Robot.java
+++ src/com/kamotelabs/tutorial43/Robot.java
@@ -1,24 +1,30 @@
 package com.kamotelabs.tutorial43;
 
 public class Robot {
-	
-	class Brain {
+
+	private class Brain {
 		public void think() {
 			System.out.println("Robot " + id + " is thinking.");
 		}
 	}
 	
+	public class Legs {
+		public void run() {
+			System.out.println("Robot " + id + " is running.");
+		}
+	}
+
 	private int id;
-	
+
 	public Robot(int id) {
 		this.id = id;
 	}
-	
+
 	public void start() {
 		System.out.println("Starting robot " + id);
-		
+
 		Brain brain = new Brain();
-		
+
 		brain.think();
 	}
 
