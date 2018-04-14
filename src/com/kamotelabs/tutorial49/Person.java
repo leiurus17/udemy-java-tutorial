fd2224bbf4775d0bc60b99c21a92a2fe83b87f94
Marlon <martianworm17@gmail.com>
2018-04-14 14:53:37 +0800

    Passing by Value

---
+++ src/com/kamotelabs/tutorial49/Person.java
@@ -0,0 +1,23 @@
+package com.kamotelabs.tutorial49;
+
+public class Person {
+	private String name;
+	
+	public Person(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Person [name=" + name + "]";
+	}
+
+}
