1b3ef1e7af210a44489350963f9be359131e1b71
Marlon <martianworm17@gmail.com>
2018-04-14 12:07:06 +0800

    Serializing Arrays

--- src/com/kamotelabs/tutorial47/ReadObjects.java
+++ src/com/kamotelabs/tutorial47/ReadObjects.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 public class ReadObjects {
 	
@@ -16,7 +17,21 @@ public class ReadObjects {
 			
 			Person[] people = (Person[]) os.readObject();
 			
+			@SuppressWarnings("unchecked")
+			ArrayList<Person> peopleList = (ArrayList<Person>) os.readObject();
+			
 			for(Person person: people) {
+				System.out.println(person);
+			}
+			
+			for(Person person: peopleList) {
+				System.out.println(person);
+			}
+			
+			int num = os.readInt();
+			
+			for(int i=0; i<num; i++) {
+				Person person = (Person) os.readObject();
 				System.out.println(person);
 			}
 			
--- src/com/kamotelabs/tutorial47/WriteObjects.java
+++ src/com/kamotelabs/tutorial47/WriteObjects.java
@@ -4,6 +4,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WriteObjects {
 	
@@ -12,11 +14,21 @@ public class WriteObjects {
 		
 		Person[] people = {new Person(1, "Sue"), new Person(99, "Mike"), new Person(77, "Bob")};
 		
+		ArrayList<Person> peopleList = new ArrayList<Person>(Arrays.asList(people));
+		
 		try (FileOutputStream fs = new FileOutputStream("array.ser")) {
 			
 			ObjectOutputStream os = new ObjectOutputStream(fs);
 			
 			os.writeObject(people);
+			
+			os.writeObject(peopleList);
+			
+			os.writeInt(peopleList.size());
+			
+			for(Person person: peopleList) {
+				os.writeObject(person);
+			}
 			
 			os.close();
 			
