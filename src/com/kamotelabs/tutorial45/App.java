bd60813ca52e73650f42dae14c1eb06cb5c300b0
Marlon <martianworm17@gmail.com>
2018-04-14 08:17:35 +0800

    Recursion: A Useful Trick Up Your Sleeve

---
+++ src/com/kamotelabs/tutorial45/App.java
@@ -0,0 +1,13 @@
+package com.kamotelabs.tutorial45;
+
+public class App {
+	
+	public static void main(String[] args) {
+		calculate();
+	}
+	
+	private static void calculate() {
+		System.out.println("Hello");
+	}
+
+}
