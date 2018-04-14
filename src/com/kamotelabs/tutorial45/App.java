3531d78faddf580eaf5707a770dbc24bd9908d58
Marlon <martianworm17@gmail.com>
2018-04-14 09:11:01 +0800

    Recursion: A Useful Trick Up Your Sleeve

--- src/com/kamotelabs/tutorial45/App.java
+++ src/com/kamotelabs/tutorial45/App.java
@@ -4,17 +4,15 @@ public class App {
 	
 	public static void main(String[] args) {
 		
-		int value = 4;
+		calculate(4);
 		
-		calculate(value);
-		
-		System.out.println(value);
 	}
 	
 	private static void calculate(int value) {
-		
-		value = value - 1;
 		System.out.println(value);
+		
+		calculate(value);
+		
 	}
 
 }
