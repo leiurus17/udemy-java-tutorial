68dfd99002795f3424c78626c6a3e6ed59edd97b
Marlon <martianworm17@gmail.com>
2018-04-14 12:49:16 +0800

    The Transient Keyword and More Serialization
    Just add a note about what is transient keyword is.

--- src/com/kamotelabs/tutorial48/Person.java
+++ src/com/kamotelabs/tutorial48/Person.java
@@ -8,7 +8,7 @@ public class Person implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private transient int id;
+	private transient int id; // use transient keyword to prevent things from being serialized
 	private String name;
 	
 	public Person(int id, String name) {
